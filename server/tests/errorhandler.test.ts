8eca25dfc55e4b66f9ee148217118a3c11b83df7
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-10 10:43:39 +0200

    test: update test errorhandler

--- server/tests/errorhandler.test.ts
+++ server/tests/errorhandler.test.ts
@@ -17,6 +17,17 @@ app.get('/error-no-status', (req: Request, res: Response, next: NextFunction) =>
   next(error);
 });
 
+app.get('/error-with-statusCode', (req: Request, res: Response, next: NextFunction) => {
+  const error: any = new Error('Test Error');
+  error['statusCode'] = 401;
+  next(error);
+});
+
+app.get('/error-no-message', (req: Request, res: Response, next: NextFunction) => {
+  const error: any = {};
+  next(error);
+});
+
 
 app.use(ErrorHandler);
 
@@ -24,25 +35,22 @@ app.use(ErrorHandler);
 describe('ErrorHandler middleware', () => {
 
   it('handles an error without custom status code', async () => {
-    app.get('/error-no-status', (req: Request, res: Response, next: NextFunction) => {
-      const error: any = new Error('Test Error');
-      next(error);
-    });
     const res = await request(app).get('/error-no-status');
     expect(res.status).toBe(500);
     expect(res.body.message).toBe('Test Error');
   });
 
   it('handles an error with custom statusCode', async () => {
-    app.get('/error-with-statusCode', (req: Request, res: Response, next: NextFunction) => {
-      const error: any = new Error('Test Error');
-      error['statusCode'] = 401;
-      next(error);
-    });
     const res = await request(app).get('/error-with-statusCode');
     expect(res.status).toBe(401);
   });
 
+  it('handles an error without a message, using the default message', async () => {
+    const res = await request(app).get('/error-no-message');
+    expect(res.status).toBe(500);
+    expect(res.body.message).toBe('Ohlala, something went wrong');
+  });
+  
   it('includes stack trace in development environment', async () => {
     process.env.NODE_ENV = 'development';
     const res = await request(app).get('/error');
