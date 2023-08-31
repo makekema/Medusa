0f9c355b15bfb99053b0fc7fc970374a4e1a6b8c
makekema <hello@magdalena-keller.com>
2023-08-31 18:19:25 +0200

    feat: change socket to process.env variable

--- client/src/apiService.tsx
+++ client/src/apiService.tsx
@@ -1,6 +1,6 @@
 import { Chatroom } from "./context/ContextTypes";
 
-const API = process.env.REACT_APP_API_URL
+export const API = process.env.REACT_APP_API_URL
   ? process.env.REACT_APP_API_URL
   : 'https://medusa.fly.dev'
 
